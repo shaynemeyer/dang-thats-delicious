c8e4f77d9b127fe8260749930e610cea7b947922
Shayne Meyer <shayne.meyer@gmail.com>
2017-05-10 13:09:13 -0700

    added store controller and pug files

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/store.controller');
 
 // Do work here
-router.get('/', (req, res) => {
-  const shayne = { name: 'Shayne', age:0 }
-  //res.send('Hey! It works!');
-  //res.json(shayne)
-  res.send(req.query.name)
-});
+router.get('/', storeController.homePage);
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-})
 
 module.exports = router;
