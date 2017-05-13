13d01d092c87e17e6db8365e721122be8b823144
Shayne Meyer <shayne.meyer@gmail.com>
2017-05-13 07:14:00 -0700

    added store detail page

--- controllers/store.controller.js
+++ controllers/store.controller.js
@@ -78,3 +78,12 @@ exports.updateStore = async (req, res) => {
   req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
   res.redirect(`/stores/${store._id}/edit`);
 };
+
+exports.getStoreBySlug = async (req, res) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if(!store) {
+    return next();
+  }
+  res.render('store', { store, title: store.name });
+  // res.json(store);
+};
