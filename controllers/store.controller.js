b280361c2a928085c7b6294cab341d17edf75a80
Shayne Meyer <shayne.meyer@gmail.com>
2017-05-13 08:03:15 -0700

    finished displaying by tags. still have a bug with sorting by tags

--- controllers/store.controller.js
+++ controllers/store.controller.js
@@ -89,7 +89,12 @@ exports.getStoreBySlug = async (req, res) => {
 };
 
 exports.getStoresByTag = async (req, res) => {
-  const tags = await Store.getTagsList();
   const tag = req.params.tag;
-  res.render('tag', { tags, title: 'Tags', tag });
+  const tagQuery = tag || { $exists: true };
+
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({tags: tagQuery});
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+
+  res.render('tag', { tags, title: 'Tags', tag, stores });
 };
--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,6 @@ router.get('/stores/:id/edit', catchErrors(storeController.editStore))
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
-router.get('/tags/:tag', catchErrors(storeController.getStoreByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 module.exports = router;
