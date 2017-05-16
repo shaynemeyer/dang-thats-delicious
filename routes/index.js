6d6ae4495501e9f0e4368c040ae8e7f5ce65d834
Shayne Meyer <shayne.meyer@gmail.com>
2017-05-15 17:26:59 -0700

    added mongodb indexes and api endpoint for search

--- routes/index.js
+++ routes/index.js
@@ -49,6 +49,8 @@ router.get('/account/reset/:token', catchErrors(authController.reset));
 router.post('/account/reset/:token',
   authController.confirmedPasswords,
   catchErrors(authController.update)
-)
+);
+
+router.get('/api/search', catchErrors(storeController.searchStores));
 
 module.exports = router;
